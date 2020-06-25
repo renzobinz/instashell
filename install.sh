65f14c0d3dcba25b1d20ddbce91c9bf02b2b3203
renzobinz <67407077+renzobinz@users.noreply.github.com>
2020-06-25 01:14:06 -0500

    Update install.sh
    git clone https://github.com/thelinuxchoice/instashell
    cd instashell
    chmod +x instashell.sh
    service tor start
    sudo ./instashell.sh

--- install.sh
+++ install.sh
@@ -21,4 +21,4 @@ checkroot
 (trap '' SIGINT SIGTSTP && command -v curl > /dev/null 2>&1 || { printf >&2  "\e[1;92mInstalling cURL, please wait...\n\e[0m"; apt-get update > /dev/null && apt-get -y install curl > /dev/null || printf "\e[1;91mCurl Not installed.\n\e[0m"; }) & wait $!
 
 printf "\e[1;92mAll Requires are installed!\n\e[0m"
-
+git clone https://github.com/thelinuxchoice/instashell.git
